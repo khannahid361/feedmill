c88bf068eef6567ac3af3e9a92431594f89ded13
Kamrul <khannahid361@gmail.com>
2025-10-14 15:44:26 +0600

    profit loss updated

--- Modules/Account/Http/Controllers/Report/ProfitLossController.php
+++ Modules/Account/Http/Controllers/Report/ProfitLossController.php
@@ -8,29 +8,31 @@ use Modules\Account\Entities\Transaction;
 use Modules\Account\Entities\ChartOfAccount;
 use Modules\Setting\Entities\Warehouse;
 
-class ProfitLossController extends BaseController{
-    public function __construct(Transaction $model){
+class ProfitLossController extends BaseController
+{
+    public function __construct(Transaction $model)
+    {
         $this->model = $model;
     }
-    public function index(){
-        if(permission('profit-loss-access')){
-            $this->setPageData('Profit Loss','Profit Loss','far fa-money-bill-alt',[['name'=>'Accounts','link'=>'javascript::void(0);'],['name'=>'Report','link'=>'javascript::void(0);'],['name'=>'Profit Loss']]);
-            $warehouses = Warehouse::all();
-            return view('account::report.profit-loss.index',compact('warehouses'));
-        }else{
-            return $this->access_blocked();
-        }
+
+    public function index()
+    {
+        $this->setPageData('Profit Loss', 'Profit Loss', 'far fa-money-bill-alt', [['name' => 'Accounts', 'link' => 'javascript::void(0);'], ['name' => 'Report', 'link' => 'javascript::void(0);'], ['name' => 'Profit Loss']]);
+        $warehouses = Warehouse::all();
+        return view('account::report.profit-loss.index', compact('warehouses'));
     }
-    public function report(Request $request){
+
+    public function report(Request $request)
+    {
         if ($request->ajax()) {
             $data = [
-                'start_date'         => $request->start_date ? $request->start_date : date('Y-m-d'),
-                'end_date'           => $request->end_date ? $request->end_date : date('Y-m-d'),
-                'asset_accounts'     => ChartOfAccount::where(['type'=>'I'])->orderBy('code','asc')->get(),
-                'liability_accounts' => ChartOfAccount::where(['type'=>['E']])->orderBy('code','asc')->get(),
-                'warehouse_id'       => $request->warehouseId,
+                'start_date' => $request->start_date ? $request->start_date : date('Y-m-d'),
+                'end_date' => $request->end_date ? $request->end_date : date('Y-m-d'),
+                'asset_accounts' => ChartOfAccount::where(['type' => 'I'])->orderBy('code', 'asc')->get(),
+                'liability_accounts' => ChartOfAccount::where(['type' => ['E']])->orderBy('code', 'asc')->get(),
+                'warehouse_id' => $request->warehouseId,
             ];
-            return view('account::report.profit-loss.report',$data)->render();
+            return view('account::report.profit-loss.report', $data)->render();
         }
     }
 }
